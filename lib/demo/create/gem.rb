3b6d4340bf5b53bd78047ba5e1e7c7394b66334d
haicv01 <haicv@zigexn.vn>
2024-05-21 13:19:03 +0700

    test use the same name

--- lib/demo/create/gem.rb
+++ lib/demo/create/gem.rb
@@ -5,7 +5,8 @@ require_relative "application_record"
 
 module Master
   # Master::Zypcode
-  class Zypcode < TestGem::ApplicationRecord
+  class Zipcode < TestGem::ApplicationRecord
     self.table_name = "master_zipcodes"
+    scope :last_record, -> { order(id: :desc).limit(1) }
   end
 end
